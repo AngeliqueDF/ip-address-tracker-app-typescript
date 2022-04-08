b1849c7a1a0b94d4871e7eba186b99a8964efe45
angelique-df <faye.angelique.d@gmail.com>
2022-04-08 23:53:13 +0000

    Update app.js
    Using OOP as the app will reuse the same objects to updates variables (such as the marker's place on the map) when the user searches a new IP address.
    
    It is the best way to avoid global variables, reduce code, and limit bugs for this app.

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,34 @@ import AddressMapControl from "./AddressMapControl";
 import PopulateTable from "./PopulateTable";
 
 window.addEventListener("DOMContentLoaded", async () => {
+	const appMap = new AddressMapControl();
+	const locator = new IpAddressLocator();
+	const infoDisplay = new PopulateTable();
+
+	const locationInfo = await locator.getLocationInfo();
+
+	if (locationInfo) {
+		infoDisplay.displayFoundInfo(locationInfo.data);
+		appMap.displayAddress([
+			locationInfo.data.location.lat,
+			locationInfo.data.location.lng,
+		]);
+	}
+
+	const searchElement = document.querySelector(".search");
+	searchElement.addEventListener("submit", async function (e) {
+		e.preventDefault();
+
+		const search = e.target.searchedAddress.value;
+
+		const newLocationInfo = await locator.getLocationInfo(search);
+
+		if (newLocationInfo) {
+			infoDisplay.displayFoundInfo(newLocationInfo.data);
+			appMap.displayAddress([
+				newLocationInfo.data.location.lat,
+				newLocationInfo.data.location.lng,
+			]);
+		}
+	});
 });
