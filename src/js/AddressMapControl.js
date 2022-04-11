57e24c448bb9bdc10f89293395d5dd4089af12d1
angelique-df <faye.angelique.d@gmail.com>
2022-04-11 14:25:24 +0000

    Add JSDoc comments

--- src/js/AddressMapControl.js
+++ src/js/AddressMapControl.js
@@ -1,5 +1,12 @@
+/**
+ * A wrapper around Leaftet's L class.
+ * Controls the map element. Displays the marker and centers the map on first load.
+ * Moves the marker and recenters the map after every search.
+ */
 class AddressMapControl {
-	// The svg icon which marks the address location
+	/**
+	 * The svg icon which marks the address location
+	 */
 	#marker;
 
 	/**
@@ -7,13 +14,19 @@ class AddressMapControl {
 	 */
 	#latLng;
 
-	// The map displayed in div#map
+	/**
+	 * The map displayed in the "div#map" HTML element
+	 */
 	#map;
 
-	// The default zoom
+	/**
+	 * The default zoom. The map's zoom is reset to this value after every search.
+	 */
 	#ZOOM = 14;
 
-	// The element that will display the custom icon
+	/**
+	 * The element that will display the custom icon
+	 */
 	#svgIcon = L.divIcon({
 		html: `
     <svg xmlns="http://www.w3.org/2000/svg" width="46" height="56"><path fill-rule="evenodd" d="M39.263 7.673c8.897 8.812 8.966 23.168.153 32.065l-.153.153L23 56 6.737 39.89C-2.16 31.079-2.23 16.723 6.584 7.826l.153-.152c9.007-8.922 23.52-8.922 32.526 0zM23 14.435c-5.211 0-9.436 4.185-9.436 9.347S17.79 33.128 23 33.128s9.436-4.184 9.436-9.346S28.21 14.435 23 14.435z"/></svg>`,
@@ -27,12 +40,14 @@ class AddressMapControl {
 
 		this.displayMap();
 
-		this.#marker = L.marker(this.latLng, { icon: this.#svgIcon }).addTo(
+		this.#marker = L.marker(this.#latLng, { icon: this.#svgIcon }).addTo(
 			this.#map
 		);
 	}
 
-	// Puts the Leaflet map in the DOM
+	/**
+	 * Renders the Leaflet map in the DOM
+	 */
 	displayMap() {
 		this.#map = L.map("map", {
 			center: this.#latLng,
@@ -45,9 +60,12 @@ class AddressMapControl {
 		}).addTo(this.#map);
 	}
 
-	// Moves the marker
-	#displayMarker(latLngDisplayed) {
-		this.#marker.setLatLng(latLngDisplayed);
+	/**
+	 * Moves the marker.
+	 * @param {*} newLatLng
+	 */
+	#displayMarker(newLatLng) {
+		this.#marker.setLatLng(newLatLng);
 	}
 
 	/**
@@ -61,4 +79,3 @@ class AddressMapControl {
 }
 
 export default AddressMapControl;
-
