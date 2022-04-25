0dd8f4fc4e1f22bf822490a3d2b846a8a4617e69
angelique-df <faye.angelique.d@gmail.com>
2022-04-25 20:59:12 +0000

    Convert app.js to TypeScript, refactor
    - requesting the IP address' information is enclosed in `displayData()`.
    - new type for `IpAddressData`. Ensures the data has the correct shape wherever it is referenced in the code.

---
+++ src/js/app.ts
@@ -0,0 +1,51 @@
+"use-strict";
+import IpAddressLocator from "./IpAddressLocator";
+import AddressMapControl from "./AddressMapControl";
+import PopulateTable from "./PopulateTable";
+import DomainNameLocator from "./DomainNameLocator";
+
+const appMap = new AddressMapControl();
+const locator = new IpAddressLocator();
+const infoDisplay = new PopulateTable();
+
+export type IpAddressData = {
+	ip: string;
+	isp: string;
+	city: string;
+	district: string;
+	zipcode: string;
+	latitude: number;
+	longitude: number;
+	time_zone: { offset: number };
+};
+
+/**
+ * Inserts information received from the IP Geolocation API in the table and updates the map.
+ */
+const displayData = async (ipAddress = "") => {
+	let data: IpAddressData;
+	data = await locator.getIpAddressInfo(ipAddress);
+
+	// Populate the ".search + table" element with the relevant data
+	infoDisplay.populateTable(data);
+
+	// Update the map so it displays the new location
+	appMap.updateMap([data.latitude, data.longitude]);
+};
+
+window.addEventListener("DOMContentLoaded", async () => {
+	/**
+	 * Locates and displays the information found on the client when the page is first loaded.
+	 */
+	displayData();
+
+	const searchElement = document.querySelector(".search");
+	searchElement.addEventListener("submit", async function (e) {
+		e.preventDefault();
+
+		const search = e.target["searchedAddress"].value;
+		let ipAddress = search;
+
+		displayData(ipAddress);
+	});
+});
