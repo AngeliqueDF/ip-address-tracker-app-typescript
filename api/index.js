df9c5807608261d790540c2d8e93510aaacd6f7c
angelique-df <faye.angelique.d@gmail.com>
2022-07-16 13:47:35 +0000

    Move `app.js` code to `index.js`

--- api/index.js
+++ api/index.js
@@ -1,9 +1,32 @@
-const path = require("path");
+const app = require("express")();
 
-const http = require("http");
-const app = require("./app");
-const server = http.createServer(app);
+app.get("/api/", async (req, res) => {
+	const search = req.query.search;
 
-server.listen(process.env.PORT || 5000, () => {
-	console.log(`Server running on port ${5000}`);
+	/**
+	 * The URL constructor requires the protocol to work. We add it if it was omitted by the user.
+	 */
+	// Checks whether the user specified a protocol in their search
+	const hasProtocol = /^(https?:\/\/)/.test(search);
+	// Add the protocol if it was omitted
+	let urlWithProtocol = hasProtocol ? search : `https://${search}`;
+
+	try {
+		// This will throw an error if the domain is invalid. In that case, the client will receive the error from the catch statement.
+		const isValidDomain = new URL(urlWithProtocol);
+
+		// Using the dns Node module, we can then get the IP address of the domain
+		dns.resolve4(search, (err, addresses) => {
+			if (err) throw err;
+			// Send a response with the IP address found
+			res.json({ ip: addresses[0] });
+		});
+	} catch (error) {
+		console.log(error);
+		return res.status(404).json({
+			error: "Could not find an IP address. Make sure the domain is valid.",
+		});
+	}
 });
+
+module.exports = app;
