ebbe3b50a419e922861fb67ae47a31d7e57622b9
angelique-df <faye.angelique.d@gmail.com>
2022-07-19 19:02:23 +0000

    Remove previous version

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,11 +1,10 @@
 "use-strict";
-import IpAddressLocator from "./IpAddressLocator";
+import SearchLocator from "./SearchLocator";
 import AddressMapControl from "./AddressMapControl";
 import PopulateTable from "./PopulateTable";
-import DomainNameLocator from "./DomainNameLocator";
 
 const appMap = new AddressMapControl();
-const locator = new IpAddressLocator();
+const searchLocator = new SearchLocator();
 const infoDisplay = new PopulateTable();
 
 export type IpAddressData = {
@@ -24,13 +23,8 @@ export type IpAddressData = {
  * Called when the page is first loaded.
  */
 const displayData = async (ipAddress = "") => {
-	let data: IpAddressData = await locator.getIpAddressInfo(ipAddress);
 
-	// Populate the ".search + table" element with the relevant data
-	infoDisplay.populateTable(data);
 
-	// Update the map so it displays the new location and moves the marker to it
-	appMap.updateMap([data.latitude, data.longitude]);
 };
 
 window.addEventListener("DOMContentLoaded", async () => {
@@ -43,24 +37,5 @@ window.addEventListener("DOMContentLoaded", async () => {
 		const search = e.target["searchedAddress"].value;
 
 		// If the field is empty, the value of ip in the URL is === "", therefore the API will return information about the client.
-		if (search === "") {
-			return displayData();
-		} else {
-			// Otherwise we will use the search input to rerender the app
-			// First, check the input is a valid domain
-			const domainLocator = new DomainNameLocator();
-			if (domainLocator.isValidDomain(search)) {
-				/**
-				 * A valid URL (according to the constructor) doesn't necessarily match with an IP address we can use in the app.
-				 * `ipFromDomain` will be either null (falsy) or an object with the new data to display
-				 */
-				const ipFromDomain = await domainLocator.getIpFromDomain();
-
-				// Check we received a truthy value
-				if (ipFromDomain) {
-					displayData(ipFromDomain);
-				}
-			}
-		}
 	});
 });
